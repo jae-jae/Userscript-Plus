b3c8029fbc8eae249f2d991d77665dd31962fd44
Jaeger <JaegerCode@gmail.com>
2017-09-03 14:39:21 +0800

    ok

--- dist/show-site-all-userjs.user.js
+++ dist/show-site-all-userjs.user.js
@@ -8,7 +8,7 @@
 // @description:zh      显示当前网站的所有可用UserJS(Tampermonkey)脚本,交流QQ群:104267383
 // @description:zh-CN   显示当前网站的所有可用UserJS(Tampermonkey)脚本,交流QQ群:104267383
 // @author       Jaeger <JaegerCode@gmail.com>
-// @icon         https://greasyfork.org/assets/blacklogo16-bc64b9f7afdc9be4cbfa58bdd5fc2e5c098ad4bca3ad513a27b15602083fd5bc.png
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3ggEBCQHM3fXsAAAAVdJREFUOMudkz2qwkAUhc/goBaGJBgUtBCZyj0ILkpwAW7Bws4yO3AHLiCtEFD8KVREkoiFxZzX5A2KGfN4F04zMN+ce+5c4LMUgDmANYBnrnV+plBSi+FwyHq9TgA2LQpvCiEiABwMBtzv95RSfoNEHy8DYBzHrNVqVEr9BWKcqNFoxF6vx3a7zc1mYyC73a4MogBg7vs+z+czO50OW60Wt9stK5UKp9Mpj8cjq9WqDTBHnjAdxzGQZrPJw+HA31oulzbAWgLoA0CWZVBKIY5jzGYzdLtdE9DlcrFNrY98zobqOA6TJKHW2jg4nU5sNBpFDp6mhVe5rsvVasUwDHm9Xqm15u12o+/7Hy0gD8KatOd5vN/v1FozTVN6nkchxFuI6hsAAIMg4OPxMJCXdtTbR7JJCMEgCJhlGUlyPB4XfumozInrupxMJpRSRtZlKoNYl+m/6/wDuWAjtPfsQuwAAAAASUVORK5CYII=
 // @include        *
 // @exclude      http://www.dev/Show-Site-All-UserJS/ui.html
 // @require      https://cdn.bootcss.com/babel-core/5.6.15/browser-polyfill.min.js
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -45,7 +45,7 @@
                 </Tooltip>
 
             </div>
-            <transition name="custom-classes-transition" enter-active-class="animated jello" leave-active-class="animated bounceOutRight">
+            <transition name="custom-classes-transition" enter-active-class="animated jello" leave-active-class="animated lightSpeedOut">
                 <div v-show="showBody">
                     <Table highlight-row :columns="columns" :data="data"></Table>
                 </div>
