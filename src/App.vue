2cb5cb38ef66587eba37912a3a13b8ed347aa7e9
Jaeger <JaegerCode@gmail.com>
2017-09-05 02:39:15 +0800

    Narrow down the lower right indicator

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,13 @@
 
 <script>
     import MainTable from './components/Table.vue'
+    import Tools from './common/js/tools'
     export default {
       components: {MainTable},
       mounted: function(){
       	//旧版本无法正常工作，更新脚本
       	if(!parent.document.getElementById('jae_userscript_box')){
-      		parent.window.open('https://greasyfork.org/scripts/24508-userscript-show-site-all-userjs/code/Userscript+%20:%20Show%20Site%20All%20UserJS.user.js')
+      		Tools.installUserJs('https://greasyfork.org/scripts/24508-userscript-show-site-all-userjs/code/Userscript+%20:%20Show%20Site%20All%20UserJS.user.js')
       		
       	}
       }
