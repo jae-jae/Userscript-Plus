7d0c92d4257ec9aed06531e929fc898050fe8a86
Jaeger <JaegerCode@gmail.com>
2017-09-18 01:20:00 +0800

    add greasyfork version

--- build-userscript.js
+++ build-userscript.js
@@ -4,24 +4,36 @@ var tpl = fs.readFileSync('./userscript/tpl.js').toString();
 var code = babel.transformFileSync("./userscript/main.js").code;
 
 var nano = function(template, data) {
-        return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
-            let keys = key.split("."),
-                v = data[keys.shift()];
-            for (let i = 0, l = keys.length; i < l; i++) v = v[keys[i]];
-            return (typeof v !== "undefined" && v !== null) ? v : "";
-        });
-    };
+	return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
+		let keys = key.split("."),
+			v = data[keys.shift()];
+		for (let i = 0, l = keys.length; i < l; i++) v = v[keys[i]];
+		return (typeof v !== "undefined" && v !== null) ? v : "";
+	});
+};
 
-var ujs = nano(tpl,{
-	code:code,
-	time:(+new Date())
-});
+var renderOut = function(outFile,ljs){
+	var ujs = nano(tpl, {
+		ljs: ljs,
+		code: code,
+		time: (+new Date())
+	});
 
-var outFile = './dist/show-site-all-userjs.user.js';
+	fs.writeFile(outFile, ujs, function(err) {
+		if (err) {
+			return console.log(err);
+		}
+		return console.log('build-out:' + outFile);
+	});
+};
 
-fs.writeFile('./dist/show-site-all-userjs.user.js',ujs,function(err){
-	if(err){
-		return console.log(err);
-	}
-	return console.log( 'build-out:' + outFile);
-});
+var ljs = `// @require     https://raw.githubusercontent.com/jae-jae/l.js/master/userjs/l.userjs.min.js
+// @require     https://gist.githubusercontent.com/jae-jae/35a1833079d26e6c9d9c6d5bed982353/raw/userjs-base.js`;
+
+var ljs_GF = `// @require      https://greasyfork.org/scripts/23419-l-js/code/ljs.js?version=148792
+// @require      https://greasyfork.org/scripts/23420-userjs-base-js/code/userjs-basejs.js?version=214700`;
+
+
+renderOut('./dist/show-site-all-userjs.user.js',ljs);
+//greasyfork version
+renderOut('./dist/show-site-all-userjs.gf.user.js',ljs_GF);
--- userscript/tpl.js
+++ userscript/tpl.js
@@ -11,8 +11,7 @@
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3ggEBCQHM3fXsAAAAVdJREFUOMudkz2qwkAUhc/goBaGJBgUtBCZyj0ILkpwAW7Bws4yO3AHLiCtEFD8KVREkoiFxZzX5A2KGfN4F04zMN+ce+5c4LMUgDmANYBnrnV+plBSi+FwyHq9TgA2LQpvCiEiABwMBtzv95RSfoNEHy8DYBzHrNVqVEr9BWKcqNFoxF6vx3a7zc1mYyC73a4MogBg7vs+z+czO50OW60Wt9stK5UKp9Mpj8cjq9WqDTBHnjAdxzGQZrPJw+HA31oulzbAWgLoA0CWZVBKIY5jzGYzdLtdE9DlcrFNrY98zobqOA6TJKHW2jg4nU5sNBpFDp6mhVe5rsvVasUwDHm9Xqm15u12o+/7Hy0gD8KatOd5vN/v1FozTVN6nkchxFuI6hsAAIMg4OPxMJCXdtTbR7JJCMEgCJhlGUlyPB4XfumozInrupxMJpRSRtZlKoNYl+m/6/wDuWAjtPfsQuwAAAAASUVORK5CYII=
 // @include        *
 // @exclude      http://www.dev/Show-Site-All-UserJS/ui.html
-// @require     https://raw.githubusercontent.com/jae-jae/l.js/master/userjs/l.userjs.min.js
-// @require     https://gist.githubusercontent.com/jae-jae/35a1833079d26e6c9d9c6d5bed982353/raw/userjs-base.js
+{ljs}
 // @resource     ui     https://raw.githubusercontent.com/jae-jae/Show-Site-All-UserJS/master/dist/ui.html?_={time}
 // @resource     uiJs   https://raw.githubusercontent.com/jae-jae/Show-Site-All-UserJS/master/dist/ui.js?_={time}
 // @grant        GM_xmlhttpRequest
