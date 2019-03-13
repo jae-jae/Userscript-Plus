3beea22dcf32d48e2d1f628e7c0a7f1f0bac54e2
Tmp341 <tmp341@gmail.com>
2019-03-13 08:50:39 +0300

    Adding Turkish localization
    Adding Turkish localization after seeing #39 pull request.

---
+++ src/common/lang/tr-TR.js
@@ -0,0 +1,25 @@
+export default {
+  table: {
+    tips: 'Bu sayfa için {count} sayıda betik bulundu',
+    install: 'Yükle',
+    title: 'Başlık',
+    author: 'Yazar',
+    authorTips: '{name} ana sayfasına ulaşmak için tıkla',
+    dailyInstalls: 'Günlük',
+    updatedTime: 'Güncellendi',
+    action: 'İşlem',
+    version: 'Sürüm',
+    score: 'Puan',
+    totalInstalls: 'Toplam Yükleme',
+    description: 'Açıklama',
+    donate: 'Bağış',
+    wechat: 'Wechat',
+    alipay: 'Alipay',
+    paypal: 'PayPal',
+    closeDonate: 'Bağışı Kapat',
+    feedback: 'Geri Bildirim',
+    close: 'Tekrar Gösterme',
+    scriptInstalling: 'Betik yükleniyor...',
+    search: 'Ara'
+  }
+}
