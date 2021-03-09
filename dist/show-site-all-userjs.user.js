edb2e3c24f4cde1f524831d2e1a3f3c5e6271a44
Travis CI User <travis@example.org>
2021-03-09 01:32:46 +0000

    Travis build

--- dist/show-site-all-userjs.user.js
+++ dist/show-site-all-userjs.user.js
@@ -20,8 +20,8 @@
 // @include        *
 // @require     https://cdn.jsdelivr.net/gh/jae-jae/l.js/userjs/l.userjs.min.js
 // @require     https://greasyfork.org/scripts/23420-userjs-base-js/code/userjs-basejs.js
-// @resource     uiJs   https://cdn.jsdelivr.net/gh/jae-jae/Userscript-Plus/dist/ui.js?_=1596113955041
-// @resource     ui     https://cdn.jsdelivr.net/gh/jae-jae/Userscript-Plus/dist/ui.html?_=1596113955041
+// @resource     uiJs   https://cdn.jsdelivr.net/gh/jae-jae/Userscript-Plus/dist/ui.js?_=1615253549298
+// @resource     ui     https://cdn.jsdelivr.net/gh/jae-jae/Userscript-Plus/dist/ui.html?_=1615253549298
 // @resource     count  https://greasyfork.org/scripts/by-site.json
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getResourceText
